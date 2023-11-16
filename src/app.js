6be7d0e9538b3fb407ea24da1a09a7cfe8b9b6d9
Tomás Beckmann <147322170+tomasbeckmann@users.noreply.github.com>
2023-11-16 23:56:11 +0000

    Listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = [
+  "The dog",
+  "My grandma",
+  "His turtle",
+  "My bird",
+  "My brother",
+  "My mother",
+];
+let action = [
+  "ate",
+  "peed",
+  "crushed",
+  "broke",
+  "destroyed",
+  "burned",
+  "cutted",
+];
+let what = [
+  "my homework",
+  "the keys",
+  "the car",
+  "my wallet",
+  "my credit card",
+];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying",
+  "yesterday when i was reading",
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+  var whoRandom = who[Math.floor(Math.random() * who.length)];
+  var actionRandom = action[Math.floor(Math.random() * action.length)];
+  var whatRandom = what[Math.floor(Math.random() * what.length)];
+  var whenRandom = when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  return `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`;
+}
+
+console.log(generateExcuse());
