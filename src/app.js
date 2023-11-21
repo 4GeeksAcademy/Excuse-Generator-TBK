ba5da638a0f4b07e1026e5b46ab4c205483295c6
Tomás Beckmann <147322170+tomasbeckmann@users.noreply.github.com>
2023-11-21 12:22:16 +0000

    Actualizado scope

--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
-let who = [
-  "The dog",
-  "My grandma",
-  "His turtle",
-  "My bird",
-  "My brother",
-  "My mother",
-];
-let action = [
-  "ate",
-  "peed",
-  "crushed",
-  "broke",
-  "destroyed",
-  "burned",
-  "cutted",
-];
-let what = [
-  "my homework",
-  "the keys",
-  "the car",
-  "my wallet",
-  "my credit card",
-];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying",
-  "yesterday when i was reading",
-];
-
 function generateExcuse() {
+  let who = [
+    "The dog",
+    "My grandma",
+    "His turtle",
+    "My bird",
+    "My brother",
+    "My mother",
+  ];
+  let action = [
+    "ate",
+    "peed",
+    "crushed",
+    "broke",
+    "destroyed",
+    "burned",
+    "cutted",
+  ];
+  let what = [
+    "my homework",
+    "the keys",
+    "the car",
+    "my wallet",
+    "my credit card",
+  ];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+    "yesterday when i was reading",
+  ];
+
   var whoRandom = who[Math.floor(Math.random() * who.length)];
   var actionRandom = action[Math.floor(Math.random() * action.length)];
   var whatRandom = what[Math.floor(Math.random() * what.length)];
